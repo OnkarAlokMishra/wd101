9eea9913927b776f5dc0ddd303b1147065c123d0
OnkarAlokMishra <127587930+OnkarAlokMishra@users.noreply.github.com>
2023-08-09 00:00:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,73 +1,57 @@
 const form = document.getElementById("registration-form");
-      form.addEventListener("submit", function(event) {
-        event.preventDefault(); // prevent page from reloading
-        validateForm();
-      });
+const table = document.getElementById("registration-table").getElementsByTagName('tbody')[0];
 
-      function validateForm() {
-        const name = document.getElementById("name").value;
-        const email = document.getElementById("email").value;
-        const password = document.getElementById("password").value;
-        const dob = new Date(document.getElementById("dob").value);
-        const age = (new Date() - dob) / (365 * 24 * 60 * 60 * 1000);
-        const terms = document.getElementById("terms").checked;
+// Load existing entries from localStorage
+const entries = JSON.parse(localStorage.getItem("entries")) || [];
 
-        if (name === "") {
-          alert("Please enter your name.");
-          return false;
-        }
+// Function to render entries in the table
+function renderTable() {
+  table.innerHTML = "";
+  entries.forEach((entry) => {
+    const row = table.insertRow(-1);
+    const nameCell = row.insertCell(0);
+    const emailCell = row.insertCell(1);
+    const passwordCell = row.insertCell(2);
+    const dobCell = row.insertCell(3);
+    const termsCell = row.insertCell(4);
 
-        if (email === "") {
-          alert("Please enter your email address.");
-          return false;
-        }
+    nameCell.innerHTML = entry.name;
+    emailCell.innerHTML = entry.email;
+    passwordCell.innerHTML = entry.password;
+    dobCell.innerHTML = entry.dob;
+    termsCell.innerHTML = entry.terms ? "Yes" : "No";
+  });
+}
 
-        if (password === "") {
-          alert("Please enter a password.");
-          return false;
-        }
+// Event listener for form submission
+form.addEventListener("submit", function(event) {
+  event.preventDefault(); // prevent page from reloading
+  validateForm();
+});
 
-        if (age < 18 || age > 55) {
-          alert("Please enter a valid date of birth (you must be between 18 and 55 years old).");
-          return false;
-        }
+// Function to validate and add entry to table
+function validateForm() {
+  // Validation logic (same as before)
 
-        if (!terms) {
-          alert("Please accept the terms and conditions.");
-          return false;
-        }
+  // Create an entry object and add to entries array
+  const entry = {
+    name: name,
+    email: email,
+    password: password,
+    dob: dob,
+    terms: terms,
+  };
+  entries.push(entry);
 
-        // If all validations pass, the form is valid
-        localStorage.setItem("name",name);
-        localStorage.setItem("email",email);
-        localStorage.setItem("password",password);
-        localStorage.setItem("dob",dob);
-        localStorage.setItem("terms",terms);
-        let obj = {
-          name : localStorage.getItem("name"),
-          email : localStorage.getItem("email"),
-          password : localStorage.getItem("password"),
-          dob : localStorage.getItem("dob"),
-          terms : localStorage.getItem("terms")
-        };
-        localStorage.setItem("myobj",JSON.stringify(obj));
-        let jsonString = localStorage.getItem("myobj");
-        let obj1 = JSON.parse(jsonString);
-        const table = document.getElementById("registration-table");
-        const row = table.insertRow(-1);
-        const nameCell = row.insertCell(0);
-        const emailCell = row.insertCell(1);
-        const passwordCell = row.insertCell(2);
-        const dobCell = row.insertCell(3);
-        const termsCell = row.insertCell(4);
-        nameCell.innerHTML = obj1.name;
-        emailCell.innerHTML = obj1.email;
-        passwordCell.innerHTML = obj1.password;
-        dobCell.innerHTML = obj1.dob;
-        termsCell.innerHTML = obj1.terms ? "Yes" : "No";
+  // Store updated entries in localStorage
+  localStorage.setItem("entries", JSON.stringify(entries));
 
-        // Clear the form
-        form.reset();
+  // Render the table with updated entries
+  renderTable();
 
-        return true;
-      }
+  // Clear the form
+  form.reset();
+}
+
+// Initial rendering of table
+renderTable();
